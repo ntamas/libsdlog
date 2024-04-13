d5f7c941325ffb66409aa331ccd1793cbbddb7b7
Tamas Nepusz <ntamas@gmail.com>
2024-04-13 23:43:27 +0200

    feat: added version header

---
+++ include/sdlog/version.h
@@ -0,0 +1,33 @@
+/**
+ * \file version.h
+ * \brief Version information for \c libsdlog
+ */
+
+#ifndef SDLOG_VERSION_H
+#define SDLOG_VERSION_H
+
+/**
+ * \def SDLOG_VERSION_MAJOR
+ * Major version number of \c libsdlog
+ */
+#define SDLOG_VERSION_MAJOR 0
+
+/**
+ * \def SDLOG_VERSION_MINOR
+ * Minor version number of \c libsdlog
+ */
+#define SDLOG_VERSION_MINOR 0
+
+/**
+ * \def SDLOG_VERSION_PATCH
+ * Patch level of \c libsdlog
+ */
+#define SDLOG_VERSION_PATCH 0
+
+/**
+ * \def SDLOG_VERSION
+ * Unified version number of \c libsdlog
+ */
+#define SDLOG_VERSION (SDLOG_VERSION_MAJOR * 10000 + SDLOG_VERSION_MINOR * 100 + SDLOG_VERSION_PATCH)
+
+#endif
